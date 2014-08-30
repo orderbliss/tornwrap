e747a65221195bc726c5e555a170c84b0a9b3b2a
stevepeak <steve@stevepeak.net>
2014-08-30 07:35:43 -0400

    added timestring validations

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,5 +1,6 @@
 import unittest
 import valideer
+import timestring
 from valideer import ValidationError as error
 
 from tornwrap.validators import *
@@ -23,3 +24,35 @@ class Test(unittest.TestCase):
 
         for x in ('not', None, 'random', object(), int):
             self.assertRaises(error, schema.validate, dict(value=x))
+
+    def test_email(self):
+        schema = valideer.parse({"email": "email"})
+        for email in ('joe@example.com', 'hello@codecov.io', 'some123_!@a.b.cc.com'):
+            self.assertEqual(schema.validate(dict(email=email))['email'], email.lower())
+
+        for email in ('needsanat', 'nodom@fefe'):
+            self.assertRaises(error, schema.validate, dict(email=email))
+
+    def test_callable(self):
+        schema = valideer.parse({"callable": "callable"})
+
+        def callback(): pass
+
+        class _object(object):
+            def __call__(self): pass
+
+        for _callable in (callback, _object()):
+            self.assertEqual(schema.validate(dict(callable=_callable))['callable'], _callable)
+
+        for _callable in ('needsanat', 1):
+            self.assertRaises(error, schema.validate, dict(callable=_callable))
+
+    def test_date(self):
+        schema = valideer.parse({"date": "date"})
+        for date in ('jan 15th 2015', 'tomorrow at 10:30', 'last tuesday'):
+            self.assertEqual(schema.validate(dict(date=date))['date'], timestring.Date(date))
+
+    def test_daterange(self):
+        schema = valideer.parse({"daterange": "daterange"})
+        for daterange in ('2 weeks', 'this year', 'next thursday'):
+            self.assertEqual(schema.validate(dict(daterange=daterange))['daterange'], timestring.Range(daterange))
