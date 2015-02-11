8136844245e11f3b39dbf25e8cabe530b1190fc3
stevepeak <steve@stevepeak.net>
2015-02-11 09:58:03 -0600

    fix parsing out sensative data

--- Makefile
+++ Makefile
@@ -4,6 +4,6 @@ test:
 	. venv/bin/activate; nosetests --rednose --with-cov --cov-config=.coveragerc
 
 upload:
-	git tag -a v$(shell python -c "import tornwrap;print tornwrap.version;") -m ""
-	git push origin v$(shell python -c "import tornwrap;print tornwrap.version;")
-	python setup.py sdist upload
+	. venv/bin/activate; git tag -a v$(shell python -c "import tornwrap;print tornwrap.version;") -m ""
+	. venv/bin/activate; git push origin v$(shell python -c "import tornwrap;print tornwrap.version;")
+	. venv/bin/activate; python setup.py sdist upload
--- tornwrap/logger.py
+++ tornwrap/logger.py
@@ -10,28 +10,37 @@ from traceback import format_exception
 from tornado.web import RedirectHandler
 from tornado.web import StaticFileHandler
 
-DEBUG = (os.getenv('DEBUG') == 'TRUE')
+DEBUG =          (str(os.getenv('DEBUG','FALSE')).upper() == 'TRUE')
+FILTER_SECRETS = re.compile(r'(?P<key>\w*secret|token|auth|password\w*\=)(?P<secret>[^\&\s]+)')
+
 if DEBUG:
-    from pygments import highlight
-    from pygments.lexers import JsonLexer
-    from pygments.lexers import PythonLexer
-    from pygments.formatters import TerminalFormatter
-    python_lexer, json_lexer, formatter = PythonLexer(), JsonLexer(), TerminalFormatter()
+    try:
+        from pygments import highlight
+        from pygments.lexers import JsonLexer
+        from pygments.lexers import PythonLexer
+        from pygments.formatters import TerminalFormatter
+        python_lexer, json_lexer, formatter = PythonLexer(), JsonLexer(), TerminalFormatter()
+    except:
+        highlight = lambda a, b, c: a
+        python_lexer, formatter = None, None
 
 _log = logging.getLogger()
 
 
-if os.getenv('LOGENTRIES_TOKEN'):
+try:
+    assert os.getenv('LOGENTRIES_TOKEN')
     from logentries import LogentriesHandler
     _log = logging.getLogger('logentries')
     _log.setLevel(getattr(logging, os.getenv('LOGLVL', "INFO")))
     _log.addHandler(LogentriesHandler(os.getenv('LOGENTRIES_TOKEN')))
-
-else:
+except:
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.DEBUG)
     _log.addHandler(ch)
     _log.setLevel(getattr(logging, os.getenv('LOGLVL', "INFO")))
+
+
+setLevel = _log.setLevel
 
 
 def json_defaults(obj):
@@ -41,7 +50,11 @@ def json_defaults(obj):
         return str(obj)
     else:
         return repr(obj)
-    
+
+
+def clean(data):
+    return FILTER_SECRETS.sub(r'\g<key>=secret', data)
+
 
 def traceback(exc_info=None, *args, **kwargs):
     if not exc_info:
@@ -70,7 +83,7 @@ def log(*args, **kwargs):
             _log.info("\033[90mLOG\033[0m %s \033[90m%s\033[0m via \033[95m%s()\033[0m" % (info.filename, str(info.lineno), info.function))
             _log.info(highlight(dumps(d, indent=2, sort_keys=True, default=json_defaults), json_lexer, formatter))
         else:
-            _log.info(dumps(d, default=json_defaults, sort_keys=True))
+            _log.info(clean(dumps(d, default=json_defaults, sort_keys=True)))
         if _debug:
             debug(_debug)
     except:
@@ -105,12 +118,6 @@ def debug(message=None, *args, **kwargs):
         traceback()
 
 
-
-setLevel = _log.setLevel
-
-FILTER_SECRETS = re.compile(r'(?P<key>\w*secret|token|auth|password\w*\=)(?P<secret>[^\&]+)')
-
-
 def handler(handler):
     if isinstance(handler, (StaticFileHandler, RedirectHandler)):
         return
@@ -118,7 +125,7 @@ def handler(handler):
     # Build log json
     _basics = {"status":    handler.get_status(),
                "method":    handler.request.method,
-               "uri":       FILTER_SECRETS.sub(r'\g<key>secret', handler.request.uri),
+               "uri":       handler.request.uri,
                "reason":    handler._reason,
                "ms":        "%.0f" % (1000.0 * handler.request.request_time())}
 
@@ -137,8 +144,8 @@ def handler(handler):
             add = "\033[92m%(method)s %(status)s\033[0m " % _basics
         
     if _basics['status'] > 499:
-        _log.fatal("%s%s"%(add, dumps(_basics)))
+        _log.fatal("%s%s"%(add, clean(dumps(_basics))))
     elif _basics['status'] > 399:
-        _log.warn("%s%s"%(add, dumps(_basics)))
+        _log.warn("%s%s"%(add, clean(dumps(_basics))))
     else:
-        _log.info("%s%s"%(add, dumps(_basics)))
+        _log.info("%s%s"%(add, clean(dumps(_basics))))
