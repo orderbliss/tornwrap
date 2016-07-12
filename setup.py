d6efa5e9d1e71e0fa3982920ca760a53e2e679a3
Steve Peak <steve@stevepeak.net>
2016-07-12 12:10:33 -0400

    v++

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: Software Development :: Testing']
 
 setup(name='tornwrap',
-      version='0.4.0a2',
+      version='0.4.0a3',
       description='tornado decorators and wrappers',
       long_description=None,
       classifiers=classifiers,
